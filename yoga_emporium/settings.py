03ac82f1f7ed305a768c52a56caa9e607fd8efe3
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-11-07 14:22:55 +0000

    Added main page header

--- yoga_emporium/settings.py
+++ yoga_emporium/settings.py
@@ -88,7 +88,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Below code credits to Code Institutes Boutique Ado project
+# Below settings credits to Code Institute's Boutique Ado project
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
