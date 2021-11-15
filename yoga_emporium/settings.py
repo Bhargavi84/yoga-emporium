64c56e2d0965d350280fd15c1eb4f3330092dc5f
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-11-15 11:51:38 +0000

    Create new app in Heroku database and connect to Postgress

--- yoga_emporium/settings.py
+++ yoga_emporium/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,11 +121,14 @@ WSGI_APPLICATION = 'yoga_emporium.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
