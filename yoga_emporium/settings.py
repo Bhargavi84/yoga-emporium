d0c972613c466f1c71285d39c35080fcc2aea229
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-11-15 13:03:27 +0000

    Removed secret key and set debug

--- yoga_emporium/settings.py
+++ yoga_emporium/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-li07t+11-ts@zn$etd3jn+xkd)=wjbt*+#=+6v!gz_ba+b@goq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bhargavi84-yoga-emporium-ms4.herokuapp.com', 'localhost']
 
