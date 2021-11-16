75950da95e0ef916882beebed2dc06b8497093b7
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-11-16 12:25:56 +0000

    Testing Debug set off

--- yoga_emporium/settings.py
+++ yoga_emporium/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'STRIPE_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bhargavi84-yoga-emporium-ms4.herokuapp.com', 'localhost']
 
